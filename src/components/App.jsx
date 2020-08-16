d7e720a2abfe210cd7897c3c032e3b9a0d39d1f6
Animish Pande <63346282+animishpande@users.noreply.github.com>
2020-08-16 23:28:52 +0530

    Not-completed-yet

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function HandleChange(event) {
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          onChange={HandleChange}
+          name="fName"
+          placeholder="First Name"
+          value={fullName.fName}
+        />
+        <input
+          onChange={HandleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={fullName.lName}
+        />
         <button>Submit</button>
       </form>
     </div>
